492a1b575738f054b2a920da79a71bf4f49e4d22
accrane <austincrane@gmail.com>
2022-06-01 11:21:01 -0400

    jobs

--- inc/post-types.php
+++ inc/post-types.php
@@ -95,81 +95,113 @@ function js_custom_init() {
         }
     }
 }
-add_action( 'init', 'ii_custom_taxonomies', 0 );
-function ii_custom_taxonomies() {
-        $posts = array(
-            array(
-                'post_type' => 'post',
-                'menu_name' => 'Activity',
-                'plural'    => 'Activities',
-                'single'    => 'Activity',
-                'taxonomy'  => 'activity_type',
-                'rewrite'   => 'activity-type'
-            ),
-            array(
-              'post_type' => array('job'),
-              'menu_name' => 'Job Types',
-              'plural'    => 'Job Types',
-              'single'    => 'Job Type',
-              'taxonomy'  => 'jobtype',
-              'query_var' => true
-            ),
-            array(
-              'post_type' => array('job'),
-              'menu_name' => 'Job Locations',
-              'plural'    => 'Job Locations',
-              'single'    => 'Job Location',
-              'taxonomy'  => 'joblocation',
-              'query_var' => true
-            ),
-            array(
-              'post_type' => array('job'),
-              'menu_name' => 'Departments',
-              'plural'    => 'Departments',
-              'single'    => 'Department',
-              'taxonomy'  => 'department',
-              'query_var' => true
-            ),
-        );
-    
-    if($posts) {
-        foreach($posts as $p) {
-            $p_type = ( isset($p['post_type']) && $p['post_type'] ) ? $p['post_type'] : ""; 
-            $single_name = ( isset($p['single']) && $p['single'] ) ? $p['single'] : "Custom Post"; 
-            $plural_name = ( isset($p['plural']) && $p['plural'] ) ? $p['plural'] : "Custom Post"; 
-            $menu_name = ( isset($p['menu_name']) && $p['menu_name'] ) ? $p['menu_name'] : $p['plural'];
-            $taxonomy = ( isset($p['taxonomy']) && $p['taxonomy'] ) ? $p['taxonomy'] : "";
-            $rewrite = ( isset($p['rewrite']) && $p['rewrite'] ) ? $p['rewrite'] : $taxonomy;
-            
-            
-            if( $taxonomy && $p_type ) {
-                $labels = array(
-                    'name' => _x( $menu_name, 'taxonomy general name' ),
-                    'singular_name' => _x( $single_name, 'taxonomy singular name' ),
-                    'search_items' =>  __( 'Search ' . $plural_name ),
-                    'popular_items' => __( 'Popular ' . $plural_name ),
-                    'all_items' => __( 'All ' . $plural_name ),
-                    'parent_item' => __( 'Parent ' .  $single_name),
-                    'parent_item_colon' => __( 'Parent ' . $single_name . ':' ),
-                    'edit_item' => __( 'Edit ' . $single_name ),
-                    'update_item' => __( 'Update ' . $single_name ),
-                    'add_new_item' => __( 'Add New ' . $single_name ),
-                    'new_item_name' => __( 'New ' . $single_name ),
-                  );
+/* ##########################################################
+ * Add new taxonomy, make it hierarchical (like categories)
+ * Custom Taxonomies
+*/
+add_action( 'init', 'build_taxonomies', 0 ); 
+function build_taxonomies() {
 
-              register_taxonomy($taxonomy,array($p_type), array(
-                'hierarchical' => true,
-                'labels' => $labels,
-                'show_ui' => true,
-                'show_in_rest' => true,
-                'show_admin_column' => true,
-                'query_var' => true,
-                'rewrite' => array( 'slug' => $rewrite ),
-              ));
-            }
-            
-        }
+  $post_types = array(
+    array(
+      'post_type' => array('music'),
+      'menu_name' => 'Location',
+      'plural'    => 'Location',
+      'single'    => 'Location',
+      'taxonomy'  => 'event-location'
+    ),
+    array(
+      'post_type' => array('film', 'activity','post'),
+      'menu_name' => 'Activity',
+      'plural'    => 'Activities',
+      'single'    => 'Activity',
+      'taxonomy'  => 'activity_type',
+      'rewrite'   => 'activity-type',
+      'query_var' => true
+    ),
+    array(
+      'post_type' => array('faqs'),
+      'menu_name' => 'FAQ Type',
+      'plural'    => 'FAQ Types',
+      'single'    => 'FAQ Type',
+      'taxonomy'  => 'faq_type',
+      'rewrite'   => 'faq-type'
+    ),
+    array(
+      'post_type' => array('story'),
+      'menu_name' => 'Editions',
+      'plural'    => 'Editions',
+      'single'    => 'Edition',
+      'taxonomy'  => 'edition',
+      'query_var' => true
+    ),
+    array(
+      'post_type' => array('job'),
+      'menu_name' => 'Job Types',
+      'plural'    => 'Job Types',
+      'single'    => 'Job Type',
+      'taxonomy'  => 'jobtype',
+      'query_var' => true
+    ),
+    array(
+      'post_type' => array('job'),
+      'menu_name' => 'Job Locations',
+      'plural'    => 'Job Locations',
+      'single'    => 'Job Location',
+      'taxonomy'  => 'joblocation',
+      'query_var' => true
+    ),
+    array(
+      'post_type' => array('job'),
+      'menu_name' => 'Departments',
+      'plural'    => 'Departments',
+      'single'    => 'Department',
+      'taxonomy'  => 'department',
+      'query_var' => true
+    ),
+    
+  );
+
+  if($post_types) {
+    foreach($post_types as $p) {
+      $p_type = ( isset($p['post_type']) && $p['post_type'] ) ? $p['post_type'] : ""; 
+      $single_name = ( isset($p['single']) && $p['single'] ) ? $p['single'] : "Custom Post"; 
+      $plural_name = ( isset($p['plural']) && $p['plural'] ) ? $p['plural'] : "Custom Post"; 
+      $menu_name = ( isset($p['menu_name']) && $p['menu_name'] ) ? $p['menu_name'] : $p['plural'];
+      $taxonomy = ( isset($p['taxonomy']) && $p['taxonomy'] ) ? $p['taxonomy'] : "";
+      $rewrite = ( isset($p['rewrite']) && $p['rewrite'] ) ? $p['rewrite'] : $taxonomy;
+      $query_var = ( isset($p['query_var']) && $p['query_var'] ) ? $p['query_var'] : true;
+      $show_admin_column = ( isset($p['show_admin_column']) ) ? $p['show_admin_column'] : true;
+
+      $labels = array(
+        'name' => _x( $menu_name, 'taxonomy general name' ),
+        'singular_name' => _x( $single_name, 'taxonomy singular name' ),
+        'search_items' =>  __( 'Search ' . $plural_name ),
+        'popular_items' => __( 'Popular ' . $plural_name ),
+        'all_items' => __( 'All ' . $plural_name ),
+        'parent_item' => __( 'Parent ' .  $single_name),
+        'parent_item_colon' => __( 'Parent ' . $single_name . ':' ),
+        'edit_item' => __( 'Edit ' . $single_name ),
+        'update_item' => __( 'Update ' . $single_name ),
+        'add_new_item' => __( 'Add New ' . $single_name ),
+        'new_item_name' => __( 'New ' . $single_name ),
+      );
+
+      register_taxonomy($taxonomy, $p_type, array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_admin_column' => $show_admin_column,
+        'query_var' => $query_var,
+        'show_ui' => true,
+        'show_in_rest' => true,
+        'public' => true,
+        '_builtin' => true,
+        'rewrite' => array( 'slug' => $rewrite ),
+      ));
+
     }
+  }
+
 }
 // Add new taxonomy, make it hierarchical (like categories)
 // add_action( 'init', 'ii_custom_taxonomies', 0 );
