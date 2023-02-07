2c8c98f0d309ae013e82f702220e0b9d318f97ad
accrane <austincrane@gmail.com>
2023-02-07 11:53:55 -0500

    add wrapper

--- page.php
+++ page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<div class="wrapper">
 			<?php
 			while ( have_posts() ) : the_post();
 
@@ -29,10 +29,10 @@ get_header(); ?>
 
 			endwhile; // End of the loop.
 			?>
-
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
